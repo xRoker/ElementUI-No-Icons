b0b5eb3a4208f39194bbbc4e8e092472d541fbe4
Alex <xroker@gmail.com>
2017-01-19 13:07:38 +0800

    add svg icons

--- client/components/Counter.vue
+++ client/components/Counter.vue
@@ -4,13 +4,13 @@
     <md-whiteframe>
       <md-toolbar>
         <md-button class="md-icon-button">
-          <md-icon>menu</md-icon>
+          <md-icon md-src="icons/zi.svg"></md-icon>
         </md-button>
 
         <h2 class="md-title" style="flex: 1">Default</h2>
 
         <md-button class="md-icon-button">
-          <md-icon>favorite</md-icon>
+          <md-icon md-src="icons/homework.svg"></md-icon>
         </md-button>
       </md-toolbar>
     </md-whiteframe>
