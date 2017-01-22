9460abdafbb86b6655d9ad4f8fe9db9a9611ce90
Alex <xroker@gmail.com>
2017-01-22 13:34:49 +0800

    Add subscription lib

--- client/app.js
+++ client/app.js
@@ -5,6 +5,8 @@ import App from './components/App'
 // Apollo
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import VueApollo from 'vue-apollo';
+import { Client } from 'subscriptions-transport-ws'
+import { print } from 'graphql-tag/printer';
 // Vue Material
 var VueMaterial = require('vue-material')
 require('vue-material/dist/vue-material.css')
@@ -21,10 +23,17 @@ const app = new Vue({
 })
 
 // Create the apollo client
-const apolloClient = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: 'https://api.graph.cool/simple/v1/ciy2b682r0cj50147oosrzr1s',
-  }),
+const wsClient = new Client('ws://subscriptions.graph.cool/ciy2b682r0cj50147oosrzr1s');
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/ciy2b682r0cj50147oosrzr1s',
+})
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient,
+)
+
+const apolloClient = new ApolloClient({ 
+  networkInterface: networkInterfaceWithSubscriptions,
   dataIdFromObject: r => r.id,
 });
 
@@ -43,3 +52,25 @@ Vue.use(VueMaterial)
 // Vue.use(VueMaterial.MdWhiteframe)
 
 export {app, router, store}
+
+function addGraphQLSubscriptions(networkInterface, wsClient) {
+  if (!networkInterface) {
+    throw new TypeError('addGraphQLSubscriptions requires a networkInterface')
+  }
+
+  if (!wsClient) {
+    throw new TypeError('addGraphQLSubscriptions requires a wsClient')
+  }
+
+  return Object.assign(networkInterface, {
+    subscribe(request, cb) {
+      return wsClient.subscribe({
+        ...request,
+        query: print(request.query)
+      }, cb)
+    },
+    unsubscribe(id) {
+      wsClient.unsubscribe(id);
+    },
+  })
+}
